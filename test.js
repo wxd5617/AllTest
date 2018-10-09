a675cc5fa3df4ae472419b1f85fe8a9b0269d800
unknown <wuxiaodong@gwmfc.com>
2018-10-09 15:22:09 +0800

    添加项目文件。

---
+++ test.js
@@ -0,0 +1,39 @@
+﻿{
+    "REQUEST_ID": "TID8BF47AB6EDA64476973CC5F5B6EBF57E",
+        "STATUS": "OK",
+            "EXIF_ORIENTATION": 1,
+                "SIDE": "FRONT",
+                    "ROTATE": "CW270",
+                        "IMAGE_ID": "XXXXX",
+                            "INFO": {
+        "NAME": "小红",
+            "SEX": "女",
+                "NATION": "汉",
+                    "YEAR": "1990",
+                        "MONTH": "2",
+                            "DAY": "14",
+                                "ADDRESS": "北京市海淀区XX路XX号",
+                                    "NUMBER": "360722199002148230"
+    },
+    "VALIDITY": {
+        "NAME": TRUE,
+            "SEX": TRUE,
+                "BIRTHDAY": TRUE,
+                    "ADDRESS": TRUE,
+                        "NUMBER": TRUE
+    },
+}
+{
+    "REQUEST_ID": "TID8BF47AB6EDA64476973CC5F5B6EBF57E",
+        "STATUS": "OK",
+            "SIDE": "BACK",
+                "IMAGE_ID": "XXXXX",
+                    "INFO": {
+        "AUTHORITY": "宜昌市公安局西陵分局",
+            "TIMELIMIT": "20060529-20260529"
+    },
+    "VALIDITY": {
+        "AUTHORITY": TRUE,
+            "TIMELIMIT": TRUE
+    },
+}
